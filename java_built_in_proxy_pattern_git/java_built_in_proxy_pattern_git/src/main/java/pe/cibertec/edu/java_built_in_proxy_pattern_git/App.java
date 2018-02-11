6b7dc3b8e0e601febd020908ce6e469519e2f115
@Christian <guardian2093@gmail.com>
2018-02-11 18:23:12 -0500

    Commit

---
+++ java_built_in_proxy_pattern_git/java_built_in_proxy_pattern_git/src/main/java/pe/cibertec/edu/java_built_in_proxy_pattern_git/App.java
@@ -0,0 +1,13 @@
+package pe.cibertec.edu.java_built_in_proxy_pattern_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
